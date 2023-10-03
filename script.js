353f7d79f2d839d3bb7a40ae0cd3be24b25ec805
Diogo Pinto <dpintobarbosa@gmail.com>
2023-10-03 14:22:30 -0300

    refaz requisito 1

---
+++ script.js
@@ -0,0 +1,11 @@
+const backBlack = document.getElementById("black")
+backBlack.style.backgroundColor = 'black';
+
+const backRed = document.getElementById("red")
+backRed.style.backgroundColor = 'red';
+
+const backYellow = document.getElementById("yellow")
+backYellow.style.backgroundColor = 'yellow';
+  
+const backBlue = document.getElementById("blue")
+backBlue.style.backgroundColor = 'blue';
